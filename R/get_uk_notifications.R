f623ee7c0f172a69743a1f0c6cbf10e76856c6b5
Sam Abbott <s.e.abbott12@gmail.com>
2021-03-18 12:45:46 +0000

    review current code

--- R/get_uk_notifications.R
+++ R/get_uk_notifications.R
@@ -1,4 +1,5 @@
-#' Get a combined data set of two epidemiological indicators by date of notification
+#' Get a combined data set of two epidemiological indicators by date of
+#' notification
 #'
 #' @param from "cases", "admissions" or "deaths"
 #' @param to "cases", "admissions" or "deaths"
@@ -9,15 +10,18 @@
 #' @return A data frame containing primary and secondary notifications alongside
 #' the proportion of cases that were SGTF positive.
 #' @export
-#' @importFrom dplyr select mutate filter group_by slice rename summarise inner_join
+#' @importFrom dplyr select mutate filter group_by slice rename
+#' @importFrom dplyr summarise inner_join
 #' @importFrom tidyr drop_na
 #' @importFrom vroom vroom
 #' @importFrom lubridate floor_date weeks
+#' @importFrom rlang .data
+#' @importFrom stats sd
 #' @author Sam Abbott
-get_uk_notificiatons <- function(from = c("cases", "admissions", "deaths"),
+get_uk_notifications <- function(from = c("cases", "admissions", "deaths"),
                                  to = c("cases", "admissions", "deaths"),
                                  level = c("UTLA", "NHS region", "DA"),
-                                 date_from, date_to){
+                                 date_from, date_to) {
 
   ## Arguments ---------------------------------------------------------------
   from <- match.arg(from, choices = c("cases", "admissions", "deaths"))
@@ -27,61 +31,68 @@ get_uk_notificiatons <- function(from = c("cases", "admissions", "deaths"),
   ## Data --------------------------------------------------------------------
   if (level %in% c("NHS region", "DA")) {
     base_url <-
-      paste0("https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/",
-             "master/subnational/united-kingdom/")
-  }else{
+      paste0(
+        "https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/",
+        "master/subnational/united-kingdom/"
+      )
+  } else {
     base_url <-
-      paste0("https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/",
-             "master/subnational/united-kingdom-local/")
+      paste0(
+        "https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/",
+        "master/subnational/united-kingdom-local/"
+      )
   }
 
   data_path <- "/summary/reported_cases.csv"
   primary <- suppressMessages(
     vroom(paste0(base_url, from, data_path)) %>%
-    select(location = region, date, primary = confirm)
-    )
+      select(location = .data$region, .data$date, primary = .data$confirm)
+  )
 
   secondary <- suppressMessages(
     vroom(paste0(base_url, to, data_path)) %>%
-    select(location = region, date, secondary = confirm)
-    )
+      select(location = .data$region, .data$date, secondary = .data$confirm)
+  )
 
   # link datasets
   reports <- primary %>%
     inner_join(secondary, by = c("location", "date")) %>%
-      select(location, date, primary, secondary) %>%
-      mutate(week_specimen = floor_date(date, "week", week_start = 1))
+    select(.data$location, .data$date, .data$primary, .data$secondary) %>%
+    mutate(week_specimen = floor_date(.data$date, "week", week_start = 1))
 
   das <- c("England", "Wales", "Scotland", "Northern Ireland")
   if (level %in% "NHS region") {
     reports <- reports %>%
-      filter(!(location %in% das))
-  }else if (level %in% "DA") {
+      filter(!(.data$location %in% das))
+  } else if (level %in% "DA") {
     reports <- reports %>%
-      filter(location %in% das)
+      filter(.data$location %in% das)
   }
 
   reports <- reports %>%
-    filter(!(location %in% "United Kingdom"))
+    filter(!(.data$location %in% "United Kingdom"))
 
   # make normalised predictors
   reports <- reports %>%
-    mutate(normalised_primary = (primary - mean(primary)) / sd(primary),
-           time = as.numeric(date),
-           time = time - min(time),
-           time = (time - mean(time)) / sd(time))
+    mutate(
+      normalised_primary = (.data$primary - mean(.data$primary)) /
+        sd(.data$primary),
+      time = as.numeric(.data$date),
+      time = .data$time - min(.data$time),
+      time = (.data$time - mean(.data$time)) / sd(.data$time)
+    )
 
   if (!missing(date_from)) {
-    if(!is.null(date_from)) {
+    if (!is.null(date_from)) {
       reports <- reports %>%
-        filter(date >= date_from)
+        filter(.data$date >= date_from)
     }
   }
 
   if (!missing(date_to)) {
     if (!is.null(date_to)) {
       reports <- reports %>%
-        filter(date <= date_to)
+        filter(.data$date <= date_to)
     }
   }
   return(reports)
--- R/get_uk_obs_grid.R
+++ R/get_uk_obs_grid.R
@@ -9,31 +9,38 @@
 #' @importFrom dplyr mutate select
 #' @importFrom purrr map2
 #' @importFrom tidyr unnest
+#' @importFrom rlang .data
 get_uk_obs_grid <- function(date_from, date_to) {
-  obs_pairs <- NULL
   if (missing(date_from)) {
     date_from <- NULL
   }
   if (missing(date_to)) {
     date_to <- NULL
   }
-  levels <- tibble(level = c("DA", "NHS region", "UTLA"))
+  levels <- tibble(
+    level = c("DA", "NHS region", "UTLA")
+  )
 
   ratios <- tibble(
     ratio = c("cfr", "chr", "hfr"),
-    obs_pairs = list(c("cases", "deaths"),
-                     c("cases", "admissions"),
-                     c("admissions", "deaths"))
+    obs_pairs = list(
+      c("cases", "deaths"),
+      c("cases", "admissions"),
+      c("admissions", "deaths")
+    )
   )
 
   obs <- levels %>%
     mutate(ratios = list(ratios)) %>%
     unnest(cols = "ratios") %>%
-    mutate(data = map2(level, obs_pairs,
-                       ~ get_uk_notificiatons(.y[1], .y[2], level = .x,
-                                              date_from = date_from,
-                                              date_to = date_to))
-    ) %>%
-    select(-obs_pairs)
+    mutate(data = map2(
+      .data$level, .data$obs_pairs,
+      ~ get_uk_notificiatons(.y[1], .y[2],
+        level = .x,
+        date_from = date_from,
+        date_to = date_to
+      )
+    )) %>%
+    select(-.data$obs_pairs)
   return(obs)
 }
